0a10e9f03034d8cd970e00bd293a9803ed20a64b
Kylie Stewart <kylie@kyliestewart.tech>
2021-12-01 13:32:03 -0700

    Bring in data from prev version

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Home from './home'
+
 import './index.css'
-import App from './App'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Home />s
   </React.StrictMode>,
   document.getElementById('root'),
 )
