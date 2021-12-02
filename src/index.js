27f8f2e20b911fa6b0a02eb964adbc16213120d9
Kylie Stewart <kylie@kyliestewart.tech>
2021-12-01 20:50:07 -0700

    Add a temporary placeholder

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Footer } from './components/Footer'
-import { SiteTitle } from './constants'
+import { Header } from './components/Header'
 
 import './index.css'
 
 ReactDOM.render(
   <React.StrictMode>
-    <div>
-      <header>{SiteTitle}</header>
-      <body><p>Howdy</p></body>
+    <>
+      <Header />
+      <div className="content">
+        <div className="gifWrapper">
+        <img
+          src="https://media.giphy.com/media/B8ody8egx8JkA/giphy.gif"
+          alt="Gob from Arrested Development throwing dead doves at the board of directors"
+          height="200"
+          />
+          </div>
+        <p>Almost done...</p>
+      </div>
       <Footer />
-    </div>
+    </>
   </React.StrictMode>,
   document.getElementById('root'),
 )
