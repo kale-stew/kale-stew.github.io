a0877051286b4beb31264c65bbd4f20f7ecc4bd3
Kylie Stewart <kylie@kyliestewart.tech>
2021-12-10 12:31:41 -0700

    Projects++

--- src/projects.js
+++ src/projects.js
@@ -1,14 +1,24 @@
 const Projects = [
   {
+    title: 'kylies.photos',
+    description: 'My hiking blog and climbing log.',
+    href: 'https://kylies.photos',
+  },
+  {
     title: 'kylieis.online/now',
     description:
       "What I'm reading, listening to, working on, among other things.",
     href: 'https://kylieis.online/now',
   },
   {
-    title: 'kylies.photos',
-    description: 'My hiking blog and climbing log.',
-    href: 'https://kylies.photos',
+    title: 'kyliestewart.tech',
+    description: 'My technical blog and professional résumé.',
+    href: 'https://kyliestewart.tech'
+  },
+  {
+    title: 'kyliestewart.tech/talks',
+    description: 'All of my technical talks and their slides.',
+    href: 'https://kyliestewart.tech/talks'
   },
 ]
 
